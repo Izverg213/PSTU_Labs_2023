66a0ceb801ac8a9f93e9f1aebae422d2ca3a8d9f
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-17 20:08:16 +0500

    Update 1_2.cpp

--- Sem_1.gitkeep/1_2.gitkeep/1_2.cpp
+++ Sem_1.gitkeep/1_2.gitkeep/1_2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float a, b, res;
+	cin >> a >> b;
+	res = a / b;
+	if (b != 0)
+	{
+		cout << res;
+	}
+	else
+	{
+		cout << "no result";
+	}
 
+	return 0;
+}
