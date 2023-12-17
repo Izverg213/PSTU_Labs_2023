dd88bc5ac02221b94adafddcbb67e6cb54bfb8d3
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-17 20:12:46 +0500

    Update 2_4.cpp

--- Sem_1.gitkeep/2_4/2_4.cpp
+++ Sem_1.gitkeep/2_4/2_4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int spaces = n / 2;
+    int stars = 1;
 
+    for (int i = 0; i < (n + 1) / 2; i++)
+    {
+        for (int j = 0; j < spaces; j++)
+        {
+            cout << " ";
+        }
+        spaces--;
+        for (int j = 0; j < stars; j++)
+        {
+            cout << "*";
+        }
+        stars += 2;
+        cout << endl;
+    }
+	return 0;
+}
