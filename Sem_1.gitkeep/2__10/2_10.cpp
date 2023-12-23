01d0e70f630dcb825ab7e6c1858fe78a0b5b3280
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-23 21:31:35 +0500

    Update 2_10.cpp

--- Sem_1.gitkeep/2__10/2_10.cpp
+++ Sem_1.gitkeep/2__10/2_10.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n, tmp, max;
 
+    cout << "Введите длину последовательности:" << endl;
+    cin >> n;
+
+    cout << "Введите первое число:" << endl;
+    cin >> max;
+
+    for (int i = 2; i <= n; i++)
+    {
+        cout << "Введите следующее число:" << endl;
+        cin >> tmp;
+        if (tmp > max)
+        {
+            max = tmp;
+        }
+    }
+    cout << "Max " << max << endl;
+}
