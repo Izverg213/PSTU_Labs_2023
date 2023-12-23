c20f24e587f49c6d2841345796f5ba35e21f1b32
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-23 23:49:40 +0500

    Update 2_18.cpp

--- Sem_1.gitkeep/2__18/2_18.cpp
+++ Sem_1.gitkeep/2__18/2_18.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k = 0, c;
+    cin >> n;
+    while (n > 0)
+    {
+        c = n % 10;
+        k = k * 10 + c;
+        n /= 10;
+    }
+    cout << k << endl;
+    return 0;
+}
