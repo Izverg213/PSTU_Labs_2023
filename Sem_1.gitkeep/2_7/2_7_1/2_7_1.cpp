bfbbf4c769f4e52e67a4782260d07bf936e3db84
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-23 19:53:28 +0500

    Update 2_7_1.cpp

--- Sem_1.gitkeep/2_7/2_7_1/2_7_1.cpp
+++ Sem_1.gitkeep/2_7/2_7_1/2_7_1.cpp
@@ -1,1 +1,17 @@
-
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n, f;
+    cin >> n;
+    f = sqrt(n);
+    for (int i = 0; i < f; i++) {
+        for (int j = 0; j < f; j++)
+        {
+            cout << '* ';
+        }
+        cout << endl;
+        }
+}
