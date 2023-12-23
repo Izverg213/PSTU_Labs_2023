16b01fc3224e3e22d735aed56330c2198b3cebcf
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2023-12-23 23:51:28 +0500

    Update 2_23.cpp

--- Sem_1.gitkeep/2__23/2_23.cpp
+++ Sem_1.gitkeep/2__23/2_23.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Посчитать количество элементов с максимальным значением в последовательности. 
+// Последовательность элементов задана формулой общего члена $a_{ i } = sin(n + \frac{ i }{n})$
+int main()
+{
+	float n, a, max;
+	int i = 2;
+	int count = 1;
+	setlocale(LC_ALL, "RU");
+    cin >> n;
+
+    max = sin(n + 1 / n);
+
+    while (i <= n)
+    {
+        a = sin(n + i / n);
+        if (a > max)
+        {
+            max = a;
+            count = 1;
+        }
+        else if (a == max)
+        {
+            count++;
+        }
+        i++;
+    }
+    cout << "Максимальный элемент: " << max << endl;
+    cout << "Число элементов с этим значением: " << count << endl;
+}
 
