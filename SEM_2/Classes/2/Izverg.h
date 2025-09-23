ea936f7eac55fac543359f91aaaa8feb290c8412
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2025-09-23 11:12:39 +0500

    Update Izverg.h

--- SEM_2/Classes/2/Izverg.h
+++ SEM_2/Classes/2/Izverg.h
@@ -1,1 +1,32 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Izverg
+{
+    string ln_fn_p;
+    string post;
+    double salary;
+
+public:
+    Izverg(string, string, double);
+
+    Izverg();
+
+    Izverg(const Collaborator&);
+
+    ~Izverg();
+
+    string get_fio();
+    double get_salary();
+    string get_post();
+    void set_fio(string);
+    void set_salary(double);
+    void set_post(string);
+
+    void show();
+
+
+};
 
