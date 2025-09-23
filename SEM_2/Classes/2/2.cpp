9543061861951287df381db215e109f531cb98b1
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2025-09-23 11:11:26 +0500

    Update 2.cpp

--- SEM_2/Classes/2/2.cpp
+++ SEM_2/Classes/2/2.cpp
@@ -1,8 +1,8 @@
 ﻿#include<iostream>
-#include "Collaborator.h"
+#include "Izverg.h"
 using namespace std;
 
-Collaborator make_payday() {
+Izverg make_payday() {
     string fio;
     string post;
     double zp;
@@ -16,29 +16,29 @@ Collaborator make_payday() {
     cout << "Введите должность\n";
     getline(cin, post);
 
-    Collaborator p(fio, post, zp);
+    Izverg p(fio, post, zp);
     return p;
 }
 
-void print_collaborator(Collaborator p) {
+void print_Izverg(Izverg p) {
     p.show();
 }
 
 int  main() {
     system("chcp 1251>null");
-    Collaborator p1;
+    Izverg p1;
     p1.show();
     cout << "\n";
 
-    Collaborator p2("Костылёв s1mple Олегович", "Скамейка запасных", 200000);
+    Izverg p2("Костылёв s1mple Олегович", "Скамейка запасных", 200000);
     p2.show();
     cout << "\n";
-    Collaborator p3(p2);
+    Izverg p3(p2);
     p3.show();
     p3.set_fio("Крышковец donk Данил");
     p3.set_post("рифлер");
     p3.set_salary(250000);
-    print_collaborator(p3);
+    print_Izverg(p3);
     cout << "\n\n";
     p1 = make_payday();
     p1.show();
