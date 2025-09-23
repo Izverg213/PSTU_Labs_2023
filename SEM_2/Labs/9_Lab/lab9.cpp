156257e6299c142bd84f0c719b727c738c334227
Izverg213 <146198243+Izverg213@users.noreply.github.com>
2025-09-23 10:27:22 +0500

    Create lab9.cpp

---
+++ SEM_2/Labs/9_Lab/lab9.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+void copySingleWordLines(const string& sourceFile, const string& destinationFile) {
+    ifstream inFile(sourceFile);
+    ofstream outFile(destinationFile);
+
+    string line;
+    while (getline(inFile, line)) {
+        istringstream iss(line);
+        string word;
+        if (iss >> word && !(iss >> word)) {
+            outFile << line << '\n';
+        }
+    }
+
+    inFile.close();
+    outFile.close();
+}
+
+string findLongestWord(const string& fileName) {
+    ifstream inFile(fileName);
+    string word, longestWord;
+
+    while (inFile >> word) {
+        if (word.size() > longestWord.size()) {
+            longestWord = word;
+        }
+    }
+
+    inFile.close();
+    return longestWord;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    const string sourceFile = "F1.txt";
+    const string destinationFile = "F2.txt";
+
+    copySingleWordLines(sourceFile, destinationFile);
+
+    string longestWord = findLongestWord(destinationFile);
+    cout << "Самое длинное слово в файле " << destinationFile << ": " << longestWord << std::endl;
+
+    return 0;
+}
